e2acb55a6d078807d2d5132499172c68ef023384
Camilo Duvane <camilowilliam0@gmail.com>
2024-10-30 12:38:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+import scrollSuaveActive from "./modules/scrollModules/scrollSuave.js";
+import scrollBotaoSobre from "./modules/scrollModules/scrollBotaoSobre.js";
+import scrollBotaoContato from "./modules/scrollModules/scrollBotaoContato.js";
+import animationScrollSection from "./modules/scrollModules/animationScrollSection.js";
+
+
+import animateCardsSection from "./modules/animateModules/animateCardsSection.js";
+import animateBtnContato from "./modules/animateModules/animateBtnContato.js";
+import animateCardsHabilidades from "./modules/animateModules/animateCardsHabilidade.js";
+import animateForm from "./modules/animateModules/animateForm.js";
+
+import initModal from "./modules/modalModules/modal.js";
+import initMenuMobile from "./modules/menuMobile/menu-mobile.js";
+import initAnimaWrite from "./modules/animateModules/write.js";
+
+
+scrollSuaveActive();
+scrollBotaoSobre();
+scrollBotaoContato();
+animationScrollSection();
+
+
+animateCardsSection();
+animateBtnContato();
+animateCardsHabilidades();
+animateForm();
+
+initModal();
+initMenuMobile();
+initAnimaWrite();
